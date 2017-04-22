e1da08d693f393444d2d7d95a97b9c2d407cf198
Thanh Tran <python0429@gmail.com>
2017-04-21 21:15:15 -0400

    Bump version

--- src/main/java/landmaster/landcore/LandCore.java
+++ src/main/java/landmaster/landcore/LandCore.java
@@ -28,7 +28,7 @@ import net.minecraftforge.oredict.*;
 public class LandCore {
 	public static final String MODID = "landcore";
 	public static final String NAME = "LandCore";
-	public static final String VERSION = "1.3.0.0";
+	public static final String VERSION = "1.3.0.1";
 	public static final String DEPENDS = "required-after:compatlayer";
 	
 	@Mod.Instance(MODID)
